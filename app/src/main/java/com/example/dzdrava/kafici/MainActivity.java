fa36ecfe51191d600d3220f947b58bd3617e4887
pemarti <martinjak.petra@gmail.com>
2018-02-27 18:09:23 +0100

    RecyclerView i CardView - bez podataka

--- app/src/main/java/com/example/dzdrava/kafici/MainActivity.java
+++ app/src/main/java/com/example/dzdrava/kafici/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.dzdrava.kafici;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Person> persons;
+    RecyclerView rv;
+
+    LinearLayoutManager llm=new LinearLayoutManager(this.getBaseContext());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.recyclerview_activity);
+
+        rv=(RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+
+        initializeData();
+        initializeAdapter();
     }
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/dzdrava/kafici/Person.java
@@ -0,0 +1,16 @@
+package com.example.dzdrava.kafici;
+
+/**
+ * Created by pemarti on 2/27/18.
+ */
+class Person {
+    String name;
+    String age;
+    int photoId;
+
+    Person(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
